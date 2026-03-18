7d2aacf1080709bc96fee2c3563c09a9f9c7c9ad
cobatary <sburdiug@gmail.com>
2026-03-18 18:40:12 +0100

    main for kr 3

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
-from task1_kr1 import run_task1
-from task2_kr1 import run_task2
+import sympy as sp
+
+from steepest_descent_constant_step import run_steepest_descent_constant_step
+from steepest_descent_optimal_step import run_steepest_descent_optimal_step
+from partan_steepest_descent_method import run_partan_mns
+from newton_multivariate_method import run_newton_multivariate_method
 
 
-def f(x):
-    """Функція f(x) = (100 - x)^2"""
-    #return (100 - x) ** 2
-    #return x**2 - 20*x
-    #return x*(2*x-3)
-    return x**2 - 5 * x
-
-
-# Основна програма
 if __name__ == "__main__":
+    # Налаштування методів у main.py
+    x1, x2 = sp.symbols("x1 x2")
+    max_iter = 3
 
-    x0 = 3.5
-    delta = 0.1
-    eps = 0.2
-    eps_golden = 0.2
-    eps_powell = 0.01
+    fx = 3 * (x1 - 4)** 2 + x1 * x2 + 7 * x2 ** 2
 
-    run_task1(f, x0, delta, eps, eps_golden, eps_powell)
-    print()
-    run_task2()
 
+
+    #fx_const = 3 * x1**2 + x1 * x2 + 2 * x2**2
+    x_start_const = (9.8, 9.8)
+    step = 1.0
+    run_steepest_descent_constant_step(
+        fx=fx,
+        x_start=x_start_const,
+        step=step,
+        max_iter=max_iter,
+        show_plot=True,
+    )
+
+    print("\n" + "=" * 60 + "\n")
+
+    #fx_opt = 4 * x1**2 + x1 * x2 + x2**2
+    x_start_opt = (9.8, 9.8)
+    run_steepest_descent_optimal_step(
+        fx=fx,
+        x_start=x_start_opt,
+        max_iter=max_iter,
+        show_plot=True,
+    )
+
+    print("\n" + "=" * 60 + "\n")
+
+    #fx_partan = 4 * x1**2 + x1 * x2 + x2**2
+    run_partan_mns(
+        fx=fx,
+        x_start=(9.8, 9.8),
+        max_iter=max_iter,
+        eps=1e-4,
+        show_plot=True,
+    )
+
+    print("\n" + "=" * 60 + "\n")
+
+    #fx_newton = 4 * x1**2 + x1 * x2 + x2**2
+    run_newton_multivariate_method(
+        fx=fx,
+        x_start=(9.8, 9.8),
+        max_iter=max_iter,
+        eps=1e-4,
+        show_plot=True,
+    )
